decf34ff659fd44b4acc94e78516372b2ceb5464
lollion <arhimedyura@gmail.com>
2020-11-06 13:28:11 +0200

    requests endpoint refactor

--- src/main/kotlin/com/lollion/controller/SuggestionsController.kt
+++ src/main/kotlin/com/lollion/controller/SuggestionsController.kt
@@ -4,12 +4,17 @@ import com.lollion.service.MailService
 import com.lollion.service.SuggestionsService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 @RequestMapping("/")
 class SuggestionsController(val mailService: MailService, val suggestionsService: SuggestionsService) {
 
+    private val counter = AtomicLong()
     private val logger: Logger = LoggerFactory.getLogger(SuggestionsController::class.java)
 
     @GetMapping("/suggestions")
@@ -20,5 +25,12 @@ class SuggestionsController(val mailService: MailService, val suggestionsService
         suggestions?.let {
             mailService.sendEmail(to = to, description = null, text = it.joinToString(transform = addPrefix))
         }
+    }
+
+    @GetMapping("/requests")
+    fun getRequestsCount(): Long? {
+        val result = counter.incrementAndGet()
+        logger.info("Request counter incremented to {}", result)
+        return result
     }
 }
